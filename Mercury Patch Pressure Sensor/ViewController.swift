fa8f2ae5104908e6a7fbc51990313c093a148b0b
Sean Mattson <sdmattson48@gmail.com>
2016-01-19 13:12:17 -0500

    Initial Commit

---
+++ Mercury Patch Pressure Sensor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mercury Patch Pressure Sensor
+//
+//  Created by Sean Mattson on 1/19/16.
+//  Copyright © 2016 Sean Mattson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
